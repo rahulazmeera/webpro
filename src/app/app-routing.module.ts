e18150a293cb3b4e89ee7923c360e50b9410615c
Rahul Azmeera <razme001@al003110135.local>
2019-01-14 09:34:39 -0600

    second page rahul is doing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ProjectsComponent} from './projects/projects.component';
+//use this when the routing concept is need in angualr 4
 
-const routes: Routes = [];
+const routes: Routes = [
+ 
+  //define paths here so it will call the exact page which we like to call
+
+ { path: 'projects', component: ProjectsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+//importing flex layout to use proper spacings between the elements.
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { ProjectsComponent } from './projects/projects.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
